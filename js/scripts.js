2ae7e3710435740e899c4d8c8cbb4e9cf9801711
Rogelio U <ulisescodi@gmail.com>
2023-01-16 17:12:11 -0600

    Scripts nuevos

--- js/scripts.js
+++ js/scripts.js
@@ -15,13 +15,62 @@ const enlaces = document.querySelectorAll(".navegacion a");
 console.log(enlaces[0]);
 
 enlaces[0].textContent = "nuevo texto para enlace";
-enlaces[0].classList.remove('navegacion__enlace');
+// enlaces[0].classList.remove('navegacion__enlace');
 
 
 //getElementById
 //Este es un forma vieja que ya no se usa muy seguido en javascript
 
-const heading2 = document.getElementById("heading");
-console.log(heading2);
+// const heading2 = document.getElementById("heading");
+// console.log(heading2);
+
+//Generar nuevo enlace.
+
+const nuevoEnlace = document.createElement('A');
+
+//agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+//Agregar el text
+nuevoEnlace.textContent = 'Un nuevo enlace';
+ 
+//Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+
+//Agregarlo al documento
+const navegacion=document.querySelector('.navegacion');
+
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
 
 
+//Eventos
+console.log(1);
+
+window.addEventListener('load', function(){ //load espera a que el Js y los archios que dependen del HTML esten listos
+    console.log(2);
+
+});
+
+window.onload=function(){
+    console.log(3);
+}
+
+document.addEventListener('DOMContentLoaded', function(){ //Solamente espera a que se descargue HTML, pero no espera a CSS o imagenes.
+    console.log(4);
+});
+
+console.log(5);
+
+window.onscroll=function(){
+    console.log('scrolling..');
+}
+
+//Seleccionar elementos y asociarles un evento
+
+const btnEnviar = document.querySelector('.boton--primario');
+btnEnviar.addEventListener('click', function(event){
+    console.log(event);
+    event.preventDefault();
+    console.log('Enviando formulario');
+})
